d089a574e877886d08ab91d845c40fb6d54fa96c
Omar Hammoud <shepherdhub@gmail.com>
2018-11-22 10:11:39 +0200

    Added a menu

---
+++ Lebanese Royale/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class Menu : MonoBehaviour {
+
+	void OnGUI(){
+		const int buttonWidth=84;
+		const int buttonHeight=60;
+		
+		if(GUI.Button(new Rect(Screen.width/2-(buttonWidth/2),(2*Screen.height/3)-(buttonHeight/2),buttonWidth,buttonHeight),"Start")){
+			Application.LoadLevel("GameScene");
+		}
+	}
+}
