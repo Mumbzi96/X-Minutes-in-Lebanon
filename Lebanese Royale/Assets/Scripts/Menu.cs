d0ca2ff043e0237e736b3e7bf7ddf16354114d0d
Omar Hammoud <shepherdhub@gmail.com>
2018-11-23 15:40:15 +0200

    Added GUISkin and changed sizes of menu buttons

--- Lebanese Royale/Assets/Scripts/Menu.cs
+++ Lebanese Royale/Assets/Scripts/Menu.cs
@@ -7,12 +7,17 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
 public class Menu : MonoBehaviour {
-		const int buttonWidth=84;
-		const int buttonHeight=60;
+	const int buttonWidth=200;
+	const int buttonHeight=84;
+	GUISkin skin;
+
+	void Start(){
+		skin= Resources.Load("GUISkin") as GUISkin;
+	}
 
 	void OnGUI(){
 		
-		
+		GUI.skin=skin;
 		if(GUI.Button(new Rect(Screen.width/2-(buttonWidth/2),(2*Screen.height/3)-(buttonHeight/2),buttonWidth,buttonHeight),"Start")){
 			Application.LoadLevel("GameScene");
 		}
--- Lebanese Royale/Assets/Scripts/Winner.cs
+++ Lebanese Royale/Assets/Scripts/Winner.cs
@@ -7,23 +7,23 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
 public class Winner : MonoBehaviour {
-	const int buttonWidth=84;
-	const int buttonHeight=60;
+	const int buttonWidth=200;
+	const int buttonHeight=84;
 	public Text WinnerText;
-	// Use this for initialization
+	GUISkin skin;
+
 	void Start () {
+		skin= Resources.Load("GUISkin") as GUISkin;
 		string winner = MainGame.Load();
 		WinnerText.text="Chicken Dinner Mr."+winner;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
 
 	void OnGUI(){
-		
-		
+		GUI.skin=skin;
 		if(GUI.Button(new Rect(Screen.width/2-(buttonWidth/2),(2*Screen.height/3)-(buttonHeight/2),buttonWidth,buttonHeight),"Go To Menu")){
 			Application.LoadLevel("Menu");
 		}
