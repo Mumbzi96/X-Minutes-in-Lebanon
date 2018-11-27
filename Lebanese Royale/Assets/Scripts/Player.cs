4b77b2ee414c501b4c8aed4664b312d0e79f0048
Omar Hammoud <shepherdhub@gmail.com>
2018-11-27 04:11:05 +0200

    Fixed turns issue and directional movement adding 1

--- Lebanese Royale/Assets/Scripts/Player.cs
+++ Lebanese Royale/Assets/Scripts/Player.cs
@@ -16,6 +16,8 @@ public class Player : MonoBehaviour {
             _points = value;
         }
     }
+	int nextX=7;
+	int nextY=0;
 
 	public void Move(int turn,string direction){
 		if (transform.position.x>(7*MainGame.tileNumbers))
@@ -29,24 +31,25 @@ public class Player : MonoBehaviour {
 	private IEnumerator MoveIt(int turn,string direction){
 		if(direction=="Left")
 			for(int i=1;i<=turn;i++){
-				transform.SetPositionAndRotation(new Vector3(transform.position.x-(7),transform.position.y,0),new Quaternion(0,0,0,0));
+				// transform.SetPositionAndRotation(new Vector3(transform.position.x-(7),transform.position.y,0),new Quaternion(0,0,0,0));
+				transform.SetPositionAndRotation(new Vector3(transform.position.x+nextX,transform.position.y+nextY,0),new Quaternion(0,0,0,0));
 				 yield return new WaitForSeconds(1f);
 			}
 		else if(direction=="Up"){
 			for(int i=1;i<=turn;i++){
-				transform.SetPositionAndRotation(new Vector3(transform.position.x,transform.position.y+(7*i),0),new Quaternion(0,0,0,0));
+				transform.SetPositionAndRotation(new Vector3(transform.position.x+nextX,transform.position.y+nextY,0),new Quaternion(0,0,0,0));
 				yield return new WaitForSeconds(1f);
 			}
 		}
 		else if(direction=="Right"){
 			for(int i=1;i<=turn;i++){
-				transform.SetPositionAndRotation(new Vector3(transform.position.x+(7),transform.position.y,0),new Quaternion(0,0,0,0));
+				transform.SetPositionAndRotation(new Vector3(transform.position.x+nextX,transform.position.y+nextY,0),new Quaternion(0,0,0,0));
 				 yield return new WaitForSeconds(1f);
 			}
 		}
 		else if(direction=="Down"){
 			for(int i=1;i<=turn;i++){
-				transform.SetPositionAndRotation(new Vector3(transform.position.x,transform.position.y-(7*i),0),new Quaternion(0,0,0,0));
+				transform.SetPositionAndRotation(new Vector3(transform.position.x+nextX,transform.position.y+nextY,0),new Quaternion(0,0,0,0));
 				yield return new WaitForSeconds(1f);
 			}
 		}
@@ -58,12 +61,19 @@ public class Player : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
+		// switch(collision.gameObject.tag){
+		// 	case "FinalFloor":MainGame.SetWinner(gameObject.tag);break;
+		// 	case "GoLeft":Move(1,"Left" );break;
+		// 	case "GoUp":Move(1,"Up");break;
+		// 	// case "GoRight":;break;
+		// 	case "GoDown":Move(1,"Down");break;
+		// }
 		switch(collision.gameObject.tag){
 			case "FinalFloor":MainGame.SetWinner(gameObject.tag);break;
-			case "GoLeft":Move(1,"Left" );break;
-			case "GoUp":Move(1,"Up");break;
-			// case "GoRight":;break;
-			case "GoDown":Move(1,"Down");break;
+			case "GoLeft":nextX=-7;nextY=0;break;
+			case "GoUp":nextX=0;nextY=7;break;
+			case "GoRight":nextX=7;nextY=0;break;
+			case "GoDown":nextX=0;nextY=-7;break;
 		}
 	}
 }
