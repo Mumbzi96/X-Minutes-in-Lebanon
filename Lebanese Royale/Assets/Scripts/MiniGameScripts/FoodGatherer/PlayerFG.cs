1a02e0ad0a216b58de282e43eef7d9adccff6885
Omar Hammoud <shepherdhub@gmail.com>
2018-11-28 18:45:48 +0200

    Updated code to be mre effecient

--- Lebanese Royale/Assets/Scripts/MiniGameScripts/FoodGatherer/PlayerFG.cs
+++ Lebanese Royale/Assets/Scripts/MiniGameScripts/FoodGatherer/PlayerFG.cs
@@ -11,13 +11,13 @@ public class PlayerFG : MonoBehaviour {
         {
             return _points;
         }
-        set
-        {
-            _points = value;
-        }
+        // set
+        // {
+        //     _points = value;
+        // }
     }
 	Rigidbody2D rb2d;
-	float speed=15;
+	float speed=7;
 
 	void Start(){
 		rb2d=gameObject.GetComponent<Rigidbody2D>();
@@ -30,38 +30,21 @@ public class PlayerFG : MonoBehaviour {
 		if(gameObject.tag=="Player1"){
 			float x= Input.GetAxis("Horizontal");
 			float y= Input.GetAxis("Vertical");
-			if (x!=null){
-				Vector3 nv3= new Vector3(x,0,0);
-				gameObject.transform.Translate(nv3*speed*Time.deltaTime);
-			}
-			if (y!=null){
-				Vector3 nv3= new Vector3(0,y,0);
-				gameObject.transform.Translate(nv3*speed*Time.deltaTime);
-			}
+			Vector3 nv3= new Vector3(x,y,0);
+			gameObject.transform.Translate(nv3*speed*Time.deltaTime);
 		}
 		else if(gameObject.tag=="Player2"){
 			float x2= Input.GetAxis("Horizontal2");
 			float y2= Input.GetAxis("Vertical2");
-			if (x2!=null){
-				Vector3 nv3= new Vector3(x2,0,0);
-				gameObject.transform.Translate(nv3*speed*Time.deltaTime);
-			}
-			if (y2!=null){
-				Vector3 nv3= new Vector3(0,y2,0);
-				gameObject.transform.Translate(nv3*speed*Time.deltaTime);
-			}
+			Vector3 nv3= new Vector3(x2,y2,0);
+			gameObject.transform.Translate(nv3*speed*Time.deltaTime);
 
 		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
 		switch(collision.gameObject.tag){
-			case "WHAT":MainGame.SetWinner(gameObject.tag);break;
-			// case "FinalFloor":MainGame.SetWinner(gameObject.tag);break;
-			// case "GoLeft":nextX=-MainGame.spacer;nextY=0;break;
-			// case "GoUp":nextX=0;nextY=MainGame.spacer;break;
-			// case "GoRight":nextX=MainGame.spacer;nextY=0;break;
-			// case "GoDown":nextX=0;nextY=-MainGame.spacer;break;
+			case "Food":_points+=collision.gameObject.GetComponent<FoodFG>().points;break;
 		}
 	}
 }
