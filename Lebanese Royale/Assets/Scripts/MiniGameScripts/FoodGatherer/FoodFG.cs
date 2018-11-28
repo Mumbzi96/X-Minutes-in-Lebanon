48fafc22164e3f6b73b54d29935e321b2bb5b6a3
Omar Hammoud <shepherdhub@gmail.com>
2018-11-28 18:40:37 +0200

    Added class for food in FoodGather minigame

--- Lebanese Royale/Assets/Scripts/MiniGameScripts/FoodGatherer/FoodFG.cs
+++ Lebanese Royale/Assets/Scripts/MiniGameScripts/FoodGatherer/FoodFG.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FoodFG : MonoBehaviour {
+	public int points = 1;
 
+	//Constructors
+	public FoodFG(int points=1){
+		this.points=points;
+
+	}
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +19,9 @@ public class FoodFG : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		Destroy(gameObject);
 	}
 }
