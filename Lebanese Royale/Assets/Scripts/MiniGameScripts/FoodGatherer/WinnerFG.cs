a9b1d6e89b598e4a01ebdac2e6661665f138486f
Omar Hammoud <shepherdhub@gmail.com>
2018-12-02 17:13:05 +0200

    Added classes for FG scenes

---
+++ Lebanese Royale/Assets/Scripts/MiniGameScripts/FoodGatherer/WinnerFG.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class WinnerFG : MonoBehaviour {
+	const int buttonWidth=200;
+	const int buttonHeight=84;
+	public Text WinnerText;
+	GUISkin skin;
+
+	void Start () {
+		skin= Resources.Load("GUISkin") as GUISkin;
+		string winner = MainGame.Load();
+		WinnerText.text="Got some points... Good job much?"+winner;
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+		GUI.skin=skin;
+		if(GUI.Button(new Rect(Screen.width/2-(buttonWidth/2),(2*Screen.height/3)-(buttonHeight/2),buttonWidth,buttonHeight),"Go To Menu")){
+			Application.LoadLevel("GameScene");
+		}
+	}
+
+}
