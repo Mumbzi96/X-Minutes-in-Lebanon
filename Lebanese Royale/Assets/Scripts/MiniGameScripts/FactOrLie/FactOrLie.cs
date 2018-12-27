67637d97cca14f72179480c4e047a87f42dbe441
Omar Hammoud <shepherdhub@gmail.com>
2018-12-27 18:11:19 +0200

    Added Save(), load() and winning criteria

--- Lebanese Royale/Assets/Scripts/MiniGameScripts/FactOrLie/FactOrLie.cs
+++ Lebanese Royale/Assets/Scripts/MiniGameScripts/FactOrLie/FactOrLie.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class FactOrLie: MonoBehaviour {
     // Points
-	int player1=0;
-    int player2=0;
+	static int player1=0;
+    static int player2=0;
 	// Text
 	public Text currentQuestionText;
 	public Text player1score;
@@ -25,8 +28,6 @@ public class FactOrLie: MonoBehaviour {
 		dialogueManager.currentQuestion=new Dialogue("NOT IMPORTANT BUT DONT DELETE",true);
 		StopAllCoroutines();
 		StartCoroutine(TypeSentence(dialogueManager.currentQuestion.question));
-		
-
     }
 
 	IEnumerator TypeSentence (string sentence){
@@ -48,9 +49,14 @@ public class FactOrLie: MonoBehaviour {
 	void Scores(){
 		player1score.text="Player1: "+player1.ToString();
 		player2score.text="Player2: "+player2.ToString();
+
+		if(player1>=10 || player2>=10){
+			inputEnabled= false;
+			Save();
+			SceneManager.LoadScene("WinnerFoL");
+		}
 	}
 
-    
     void Answer(){
         if(Input.GetKey(KeyCode.LeftArrow)){
             bool answer=true;
@@ -94,10 +100,48 @@ public class FactOrLie: MonoBehaviour {
         }
     }
 
-	/*
-		Finish()
-		Save()
-		Load()
-	*/
+	// Statics
+	public static void Save(){
+		// GameObject MainController= GameObject.FindWithTag("Player");
+		//  GetComponent<FactOrLie>()
+		Debug.Log(player1);
+		// Deciding scores
+		string winner;
+		if( player1>player2)
+			winner="1";
+		else if ( player2> player1)
+			winner="2";
+		else winner="0";
+		PlayerData data = Load();
 
+		// Actually saving
+		string savePath=Application.persistentDataPath+"/winner.dat";
+		BinaryFormatter bf = new BinaryFormatter();
+		// Adding main points to data
+		if(winner=="1")
+			data.mainp1Points+=10;
+		else if(winner=="2")
+			data.mainp2Points+=10;
+		// Saving the games last points (sa2ale)
+		data.lastp1Points= player1;
+		data.lastp2Points= player2;
+		data.lastWinner= winner;
+
+		using (var file = File.Create(savePath)){
+			bf.Serialize(file,data);
+		}
+	}
+
+	public static PlayerData Load(){
+		string savePath=Application.persistentDataPath+"/winner.dat";
+		PlayerData data=new PlayerData();
+		if (File.Exists(savePath)){
+			BinaryFormatter bf = new BinaryFormatter();
+			using (var file = File.Open(savePath, FileMode.Open)){
+				data = (PlayerData)bf.Deserialize(file);
+			}
+			return data;
+		}
+		else return new PlayerData();
+	}
 }
