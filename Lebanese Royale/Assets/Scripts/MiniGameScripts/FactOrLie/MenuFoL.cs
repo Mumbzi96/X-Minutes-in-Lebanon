e4ef75349e7c0f1090981aa45c3fa10cdd4c8b74
Omar Hammoud <shepherdhub@gmail.com>
2019-01-03 04:31:02 +0200

    FoL new menu

--- Lebanese Royale/Assets/Scripts/MiniGameScripts/FactOrLie/MenuFoL.cs
+++ Lebanese Royale/Assets/Scripts/MiniGameScripts/FactOrLie/MenuFoL.cs
@@ -7,19 +7,12 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
 public class MenuFoL : MonoBehaviour {
-	const int buttonWidth=200;
-	const int buttonHeight=84;
-	GUISkin skin;
 
 	void Start(){
-		skin= Resources.Load("GUISkin") as GUISkin;
 	}
 
-	void OnGUI(){
-		
-		GUI.skin=skin;
-		if(GUI.Button(new Rect(Screen.width/2-(buttonWidth/2),(2*Screen.height/3)-(buttonHeight/2),buttonWidth,buttonHeight),"Start MiniGame")){
-			SceneManager.LoadScene("FactOrLie");
-		}
+	public void PlayGame(){
+		SceneManager.LoadScene("FactOrLie");
 	}
+
 }
