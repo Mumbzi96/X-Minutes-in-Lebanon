f6eaf9c113579bea977f350d1b860db73ae8ca81
Omar Hammoud <shepherdhub@gmail.com>
2019-01-09 18:40:20 +0200

    Added new questions

--- Lebanese Royale/Assets/Scripts/MiniGameScripts/FactOrLie/DialogueManager.cs
+++ Lebanese Royale/Assets/Scripts/MiniGameScripts/FactOrLie/DialogueManager.cs
@@ -30,11 +30,22 @@ public class DialogueManager {
 	private void AddQuestions(){
 		dialoguesList.Add(new Dialogue("Omar made this game",true)) ;
 		dialoguesList.Add(new Dialogue("Current prime minister is Micheal Aoun",false)) ;
+		dialoguesList.Add(new Dialogue("Traffic Light are actually made for design, you don't have to use it",false)) ;
 		dialoguesList.Add(new Dialogue("You're playing",true)) ;
-		dialoguesList.Add(new Dialogue("Hyye?",true)) ;
+		dialoguesList.Add(new Dialogue("The train station acts as the main source of transportation between major cities",false)) ;
+		dialoguesList.Add(new Dialogue("Bechara El Khoury, first President and Riad El Solh, first Prime Minister of independent Lebanon are considered as the founders of the present day Lebanese Republic.",true)) ;
+		dialoguesList.Add(new Dialogue("A mother's main weapon is the famous \" mesheyye\"",true)) ;
+		dialoguesList.Add(new Dialogue("Lebanon Gained independence from France in 1943?",true)) ;
+		dialoguesList.Add(new Dialogue("Culturally, 4 kisses is the normal greeting here",false)) ; //3
+		dialoguesList.Add(new Dialogue("Insults are a form of hate",false)); // It can be, but usually love
 		dialoguesList.Add(new Dialogue("Saed Al Harriri is our current prime minister?",true));
-		dialoguesList.Add(new Dialogue("Nabih Berri is leaving his office...",false));
-		dialoguesList.Add(new Dialogue("howwe?",false));
+		dialoguesList.Add(new Dialogue("Beirut is the 2nd oldest city name in the world",true)); // The oldest
+		dialoguesList.Add(new Dialogue("Lebanon is well known for its famous phoenician desert?",false));
+		dialoguesList.Add(new Dialogue("A piece of land can have 2 owners",true));
+		dialoguesList.Add(new Dialogue("Romans gave Baalbek its current name", false)); // They called it Heliopolis
+		dialoguesList.Add(new Dialogue("\"Ask not what your country can do for you, ask what you can do for your country\" is actually a saying by Gibran Khalil Gibran", true)); //
+		dialoguesList.Add(new Dialogue("Main language of Lebanon is French, not English", false)); // Arabic...
+
 		dialogues= new Dialogue[dialoguesList.Count];
 	}
 
