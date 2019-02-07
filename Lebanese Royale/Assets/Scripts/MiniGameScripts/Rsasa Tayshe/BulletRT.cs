44651405593bb3426f0b20c0385778bb5b6b0378
Omar Hammoud <shepherdhub@gmail.com>
2019-02-07 13:26:32 +0200

    Added borders to rsasa tayshe

--- Lebanese Royale/Assets/Scripts/MiniGameScripts/Rsasa Tayshe/BulletRT.cs
+++ Lebanese Royale/Assets/Scripts/MiniGameScripts/Rsasa Tayshe/BulletRT.cs
@@ -29,7 +29,7 @@ public class BulletRT : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		if(collision.gameObject.tag!="Enemy")
+		if(collision.gameObject.tag!="Enemy"&& collision.gameObject.tag!="Ground")
 			Destroy(gameObject);
 	}
 }
